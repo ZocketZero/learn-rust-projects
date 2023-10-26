90e927ab6de230ac21ac68ab8b822a5cefe8e37e
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-10-26 12:55:21 +0700

    env_args: fix 0 / 0 = Error;

--- env_args/src/action.rs
+++ env_args/src/action.rs
@@ -26,7 +26,10 @@ pub fn times(n1: String, n2: String) -> i32 {
 }
 
 pub fn divided(n1: String, n2: String) -> i32 {
-    let num1: i32 = to_int(n1);
     let num2: i32 = to_int(n2);
+    if num2 == 0 {
+        return 0;
+    }
+    let num1: i32 = to_int(n1);
     return num1 / num2;
 }
